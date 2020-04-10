fbac2b0b88d9cb05fe078d87a08b2a4ae39d0340
Dhayvison <dhayvison.braga@hostweb.com.br>
2020-04-09 23:00:49 -0300

    nothing to see here

--- src/philosbar/Philosopher.java
+++ src/philosbar/Philosopher.java
@@ -5,15 +5,15 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Philosopher extends Thread{
-    
+public class Philosopher extends Thread {
+
     private final long id;
     private final ArrayList<Bottle> bottles;
-    
+
     private double tranquil = 0;
     private double thirsty = 0;
     private double drinking = 0;
-    
+
     private final int rounds = 6;
     private static int lastId = 0;
     private static final Random random = new Random(System.currentTimeMillis());
@@ -22,58 +22,58 @@ public class Philosopher extends Thread{
         this.bottles = new ArrayList<>();
         this.id = lastId++;
     }
-    
+
     public static int randomInRange(int min, int max) {
         return random.nextInt((max - min) + 1) + min;
     }
-    
+
     @Override
     public void run() {
-        
+
         double time;
         for (int i = 0; i < this.rounds; i++) {
             try {
                 time = System.currentTimeMillis();
-                
+
                 Thread.sleep(random.nextInt(2000)); // TRANQUIL TIME
                 this.tranquil += (System.currentTimeMillis() - time);
-                
-                time = System.currentTimeMillis(); 
+
+                time = System.currentTimeMillis();
                 System.out.println(this.id + " is THIRSTY!");
-                
+
                 drink(randomInRange(2, this.bottles.size()));
                 this.thirsty += (System.currentTimeMillis() - time); // THIRSTY TIME
-                
+
             } catch (InterruptedException ex) {
                 Logger.getLogger(Philosopher.class.getName()).log(Level.SEVERE, null, ex);
             }
-                    
+
         }
     }
 
-    private void drink(int n) throws InterruptedException{
-        
+    private void drink(int n) throws InterruptedException {
+
         int numBottles = n;
-        System.out.println(this.id+" requires "+ numBottles +" Bottles");
-        
+        System.out.println(this.id + " requires " + numBottles + " Bottles");
+
         for (Bottle bottle : bottles) {
-            bottle.await(this); 
+            bottle.await(this);
             numBottles--;
             if (numBottles == 0)
                 break;
         }
-        
-        System.out.println(TextColor.green() + id + " is DRINKING " + n +" "+TextColor.endColor() + bottles );
-        
+
+        System.out.println(TextColor.green() + id + " is DRINKING " + n + " " + TextColor.endColor() + bottles);
+
         double time = System.currentTimeMillis();
         Thread.sleep(1000); // DRINKING TIME
         this.drinking += System.currentTimeMillis() - time;
-        
+
         System.out.println(this.id + " is TRANQUIL!");
         dropBottles(n);
     }
 
-    public void dropBottles(int numBottles){
+    public void dropBottles(int numBottles) {
         for (Bottle bottle : bottles) {
             bottle.signal();
             numBottles--;
@@ -81,32 +81,32 @@ public class Philosopher extends Thread{
                 break;
         }
     }
-    
-    public void take(Bottle bottle){
+
+    public void take(Bottle bottle) {
         this.bottles.add(bottle);
     }
-    
-    public void myBottles(){
+
+    public void myBottles() {
         System.out.println(this.bottles);
     }
-    
-    public void myTimes(){
-        System.out.println("\n>> Philosopher "+this.id);
+
+    public void myTimes() {
+        System.out.println("\n>> Philosopher " + this.id);
         myBottles();
-        System.out.println("Tranquil : "+ this.tranquil/1000 +" s");
-        System.out.println("Thirsty : "+this.thirsty/1000+" s");
-        System.out.println("Average : "+(this.thirsty/this.rounds)/1000+" s");
-        System.out.println("Drinking : "+this.drinking/1000+" s");
+        System.out.println("Tranquil : " + this.tranquil / 1000 + " s");
+        System.out.println("Thirsty : " + this.thirsty / 1000 + " s");
+        System.out.println("Average : " + (this.thirsty / this.rounds) / 1000 + " s");
+        System.out.println("Drinking : " + this.drinking / 1000 + " s");
         double total = this.tranquil + this.thirsty + this.drinking;
-        System.out.println("TOTAL : "+ total/1000 +" s");
+        System.out.println("TOTAL : " + total / 1000 + " s");
     }
 
     public ArrayList<Bottle> getBottles() {
         return bottles;
     }
-    
+
     @Override
-    public long getId(){
+    public long getId() {
         return this.id;
     }
 
@@ -141,10 +141,9 @@ public class Philosopher extends Thread{
     public static void setLastId(int lastId) {
         Philosopher.lastId = lastId;
     }
-    
-    public double getAverage(){
-        return (this.thirsty/this.rounds);
+
+    public double getAverage() {
+        return (this.thirsty / this.rounds);
     }
-    
-    
+
 }
